81db886124789f763dcce3d0ad12ef7bfe38bb69
CoolL <prodangabriel.ring@gmail.com>
2021-11-29 12:10:26 +0200

    Added password hash

--- hpr/php/register.php
+++ hpr/php/register.php
@@ -3,6 +3,8 @@
     $pass = $_POST["password"];
     $email = $_POST["email"];
 
+    $pass = password_hash($pass, PASSWORD_DEFAULT);
+    
     $mysqli = new mysqli('localhost', 'root', '07122001', 'teh_web');
     
     $res = $mysqli->query("SELECT username FROM accounts WHERE username='".$name."'");
