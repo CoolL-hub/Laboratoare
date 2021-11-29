852ac133a4bb40d13bbd61624bf72085419c1469
CoolL <prodangabriel.ring@gmail.com>
2021-11-29 12:09:32 +0200

    Added login validation from server

---
+++ hpr/php/login.php
@@ -0,0 +1,30 @@
+<?php
+    $name = $_POST["login"];
+    $pass = $_POST["password"];
+    
+    $mysqli = new mysqli('localhost', 'root', '07122001', 'teh_web');
+    
+    
+    $res_user = $mysqli->query("SELECT username FROM accounts WHERE username='".$name."'");
+
+    if(mysqli_num_rows($res_user) != 0)
+    {
+        $res_pass = $mysqli->query("SELECT password FROM accounts WHERE username='".$name."'");
+
+        $res = mysqli_fetch_array($res_pass);
+
+        if (password_verify($pass, $res["password"])) {
+            // Success!
+            echo true;
+        }
+        else {
+            // Invalid credentials
+            echo false;
+        }
+    }
+    else
+    {
+        echo false;
+    }
+    $mysqli->close();
+?>
